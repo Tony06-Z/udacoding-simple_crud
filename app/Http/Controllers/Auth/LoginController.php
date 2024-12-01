93dd49701079ab9a9252cce8dd555c24d6d634ff
Tony-06 <tonizulfandy05@gmail.com>
2024-12-01 16:33:41 +0700

    forth commit

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -29,6 +30,39 @@ class LoginController extends Controller
      */
     // protected $redirectTo = '/home';
 
+    public function login(Request $request) {
+            // Validate the request
+        $request->validate([
+            'id_library' => 'required|string',
+            'password' => ['required', 'min:8'],
+        ], [
+            'id_library.required' => "ID Library harus diisi.",
+            'password.required' => "Password harus diisi.",
+            'password.min' => 'Password setidaknya harus berisi 8 karakter.',
+        ]);
+
+        // Check if the ID Library exists
+        $user = User::where('id_library', $request->id_library)->first();
+        if (!$user) {
+            return back()->withErrors([
+                'id_library' => 'ID Library yang dimasukkan tidak ditemukan.',
+            ])->withInput($request->except('password'));
+        }
+
+        // Attempt to authenticate
+        if (!Auth::attempt(['id_library' => $request->id_library, 'password' => $request->password])) {
+            return back()->withErrors([
+                'password' => 'Password yang kamu masukkan salah. Silakan coba lagi.',
+            ])->withInput($request->except('password'));
+        }
+
+        // If successful, regenerate session and redirect
+        $request->session()->regenerate();
+
+        return redirect()->intended($this->redirectTo());
+    }
+
+
     protected function redirectTo()
     {
         if (Auth::check()) {
@@ -53,6 +87,15 @@ class LoginController extends Controller
      *
      * @return void
      */
+
+    public function logout(Request $request) {
+        Auth::logout();
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+        
+        return redirect('/');
+    }
+
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
